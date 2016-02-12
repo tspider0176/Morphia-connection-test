626f85a8284a3fbd069c328482bbfde29f2f1b12
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-02-12 11:33:56 +0900

    Add Date test at testBetween

--- src/test/java/org/tspider0176/OperatorTest.java
+++ src/test/java/org/tspider0176/OperatorTest.java
@@ -8,13 +8,16 @@ import org.mongodb.morphia.Morphia;
 import org.mongodb.morphia.query.Query;
 import sun.util.resources.cldr.ebu.CalendarData_ebu_KE;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class OperatorTest{
-    // OrderByテストを行うための初期データをこの関数内で挿入する
+    // それぞれのoperatorテストを行うための初期データをこの関数内で挿入する
     @Before
     public void initialize(){
         final Morphia morphia = new Morphia();
@@ -35,10 +38,10 @@ public class OperatorTest{
         // betweenテストの為の初期データ挿入
         final Datastore mus_datastore = morphia.createDatastore(new MongoClient(), "music");
 
-        final Date cal1 = new GregorianCalendar(1995, 4, 15).getTime();
-        final Date cal2 = new GregorianCalendar(2000, 10, 3).getTime();
-        final Date cal3 = new GregorianCalendar(2005, 6, 4).getTime();
-        final Date cal4 = new GregorianCalendar(2010, 5, 29).getTime();
+        final Date cal1 = Date.from(LocalDateTime.of(1995, 4, 15, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        final Date cal2 = Date.from(LocalDateTime.of(2000, 10, 3, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        final Date cal3 = Date.from(LocalDateTime.of(2005, 6, 4, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        final Date cal4 = Date.from(LocalDateTime.of(2010, 5, 29, 0 ,0 ,0).toInstant(ZoneOffset.UTC));
 
         mus_datastore.save(new Music(1, "Oldstyle", cal1));
         mus_datastore.save(new Music(2, "Jumpstyle", cal2));
@@ -48,7 +51,7 @@ public class OperatorTest{
 
     @Test
     public void testOrderBy(){
-//MongoDB側に接続する。
+        //MongoDB側に接続する。
         final Morphia morphia = new Morphia();
         final Datastore stu_datastore = morphia.createDatastore(new MongoClient(), "student");
 
@@ -81,7 +84,87 @@ public class OperatorTest{
 
     @Test
     public void testBetween(){
-// MongoDB側に接続する。
+        // MongoDB側に接続する。
+        final Morphia morphia = new Morphia();
+        final Datastore stu_datastore = morphia.createDatastore(new MongoClient(), "student");
+        final Datastore mus_datastore = morphia.createDatastore(new MongoClient(), "music");
+
+        // Betweenクエリ
+        // 数値間でテスト
+        final Query<Student> betweenNumQuery = stu_datastore.createQuery(Student.class)
+                .filter("id >", 2)
+                .filter("id <=", 4);
+
+        // クエリを発行
+        List<Student> res1 = betweenNumQuery.asList();
+
+        // 正しいレコードが正しい数で保存されているか確かめる
+        assertThat(res1.size(), is(2));
+        assertThat(res1.get(0), is(new Student(3, "John")));
+        assertThat(res1.get(1), is(new Student(4, "Mike")));
+
+        // Betweenクエリ
+        // 日付間でテスト filterの不等号
+        final Date from = Date.from(LocalDateTime.of(2000, 1, 1, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        final Date to = Date.from(LocalDateTime.of(2010, 1, 1, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        final Query<Music> betweenDateQuery = mus_datastore.createQuery(Music.class)
+                .filter("release >", from)
+                .filter("release <=", to);
+
+        // クエリを発行
+        List<Music> res2 = betweenDateQuery.asList();
+
+        // 指定した期間に該当するレコードが取得出来たか確認
+        final Date date1 = Date.from(LocalDateTime.of(2000, 10, 3, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        final Date date2 = Date.from(LocalDateTime.of(2005, 6, 4, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        assertThat(res2.size(), is(2));
+        assertThat(res2.get(0), is(new Music(2, "Jumpstyle", date1)));
+        assertThat(res2.get(1), is(new Music(3, "Rawstyle", date2)));
+    }
+
+    //field().greaterThan()/lessThan() でbetween
+    @Test
+    public void testBetweenByDSL(){
+        // MongoDB側に接続する。
+        final Morphia morphia = new Morphia();
+        final Datastore stu_datastore = morphia.createDatastore(new MongoClient(), "student");
+        final Datastore mus_datastore = morphia.createDatastore(new MongoClient(), "music");
+
+        // Betweenクエリ idが 2 < id <= 4 を満たすレコードを取得
+        final Query<Student> betweenNumQuery = stu_datastore.createQuery(Student.class)
+                .field("id").greaterThan(2)
+                .field("id").lessThanOrEq(4);
+
+        //クエリを発行
+        List<Student> res1 = betweenNumQuery.asList();
+
+        //指定したクエリ通りのレコードが取得できるか確認
+        assertThat(res1.size(), is(2));
+        assertThat(res1.get(0), is(new Student(3, "John")));
+        assertThat(res1.get(1), is(new Student(4, "Mike")));
+
+        // Betweenクエリ releaseが 2000年1月1日以降かつ2010年1月1日未満のレコードを取得
+        final Date from = Date.from(LocalDateTime.of(2000, 1, 1, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        final Date to = Date.from(LocalDateTime.of(2010, 1, 1, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        final Query<Music> betweenDateQuery = mus_datastore.createQuery(Music.class)
+                .field("release").greaterThan(from)
+                .field("release").lessThanOrEq(to);
+
+        // クエリを発行
+        List<Music> res = betweenDateQuery.asList();
+
+        // 指定した期間に該当するレコードが取得出来たか確認
+        final Date date1 = Date.from(LocalDateTime.of(2000, 10, 3, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        final Date date2 = Date.from(LocalDateTime.of(2005, 6, 4, 0, 0, 0).toInstant(ZoneOffset.UTC));
+        assertThat(res.size(), is(2));
+        assertThat(res.get(0), is(new Music(2, "Jumpstyle", date1)));
+        assertThat(res.get(1), is(new Music(3, "Rawstyle", date2)));
+    }
+
+    //filter().filter()でbetween
+    @Test
+    public void testBetweenByOperator(){
+        // MongoDB側に接続する。
         final Morphia morphia = new Morphia();
         final Datastore stu_datastore = morphia.createDatastore(new MongoClient(), "student");
         final Datastore mus_datastore = morphia.createDatastore(new MongoClient(), "music");
@@ -98,23 +181,6 @@ public class OperatorTest{
         assertThat(res1.size(), is(2));
         assertThat(res1.get(0), is(new Student(3, "John")));
         assertThat(res1.get(1), is(new Student(4, "Mike")));
-
-        // Betweenクエリ 日付間でテスト
-        final Date from = new GregorianCalendar(2000, 1, 1).getTime();
-        final Date to = new GregorianCalendar(2010, 1, 1).getTime();
-        final Query<Music> betweenDateQuery = mus_datastore.createQuery(Music.class)
-                .field("release").greaterThan(from)
-                .field("release").lessThan(to);
-
-        // クエリを発行
-        List<Music> res2 = betweenDateQuery.asList();
-
-        // 指定した期間に該当するレコードが取得出来たか確認
-        final Date date1 = new GregorianCalendar(2000, 10, 3).getTime();
-        final Date date2 = new GregorianCalendar(2005, 6, 4).getTime();
-        assertThat(res2.size(), is(2));
-        assertThat(res2.get(0), is(new Music(2, "Jumpstyle", date1)));
-        assertThat(res2.get(1), is(new Music(3, "Rawstyle", date2)));
     }
 
     @Test
@@ -135,6 +201,25 @@ public class OperatorTest{
         assertThat(res.get(0), is(new Student(1, "Tom")));
         assertThat(res.get(1), is(new Student(3, "John")));
         assertThat(res.get(2), is(new Student(4, "Mike")));
+    }
+
+    @Test
+    public void testLimit(){
+        // MongoDB側に接続する。
+        final Morphia morphia = new Morphia();
+        final Datastore stu_datastore = morphia.createDatastore(new MongoClient(), "student");
+
+        // Limitクエリ
+        final Query<Student> limitQuery = stu_datastore.createQuery(Student.class)
+                .limit(2);
+
+        // クエリを発行
+        List<Student> res = limitQuery.asList();
+
+        // 正しいレコードが正しい数で保存されているか確かめる
+        assertThat(res.size(), is(2));
+        assertThat(res.get(0), is(new Student(1, "Tom")));
+        assertThat(res.get(1), is(new Student(2, "Jack")));
     }
 
     @Test
@@ -168,24 +253,5 @@ public class OperatorTest{
         assertThat(res2.size(), is(2));
         assertThat(res2.get(0), is(new Student(2, "Jack")));
         assertThat(res2.get(1), is(new Student(3, "John")));
-    }
-
-    @Test
-    public void testLimit(){
-        // MongoDB側に接続する。
-        final Morphia morphia = new Morphia();
-        final Datastore stu_datastore = morphia.createDatastore(new MongoClient(), "student");
-
-        // Limitクエリ
-        final Query<Student> limitQuery = stu_datastore.createQuery(Student.class)
-                .limit(2);
-
-        // クエリを発行
-        List<Student> res = limitQuery.asList();
-
-        // 正しいレコードが正しい数で保存されているか確かめる
-        assertThat(res.size(), is(2));
-        assertThat(res.get(0), is(new Student(1, "Tom")));
-        assertThat(res.get(1), is(new Student(2, "Jack")));
     }
 }
